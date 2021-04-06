b117b9ff71976dc8363a1d33c779b96955cc22e4
Pranav Kethireddy <pran.kethi@gmail.com>
2021-04-05 19:35:38 -0700

    changed the script from index.html to index.js

--- Public/index.js
+++ Public/index.js
@@ -2,3 +2,19 @@ $(document).ready(function () {
     $('.header').height($(window).height());
 
 })
+
+const history = useHistory();
+history.push("SteamClubGuestView");
+
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    var name = profile.getName();
+    var email = profile.getEmail();
+    console.log(name);
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log(email);
+    var id_token = googleUser.getAuthResponse().id_token;
+    // This is null if the 'email' scope is not present.
+}
+//        document.querySelector("#content").innerText =
+//          googleUser.getBasicProfile().getGivenName();
