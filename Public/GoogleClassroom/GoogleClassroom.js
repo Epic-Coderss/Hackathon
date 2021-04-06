cd9c318eed89e7aa3e0262ff8279409b46e7a231
shriraj20 <shriraj.dash2016@gmail.com>
2021-04-06 15:56:43 -0700

    Update GoogleClassroom.js

--- Public/GoogleClassroom/GoogleClassroom.js
+++ Public/GoogleClassroom/GoogleClassroom.js
@@ -90,8 +90,16 @@ async function runSample() {
 }
 
 const scopes = [
-    'https://www.googleapis.com/auth/contacts.readonly',
-    'https://www.googleapis.com/auth/user.emails.read',
+    'https://www.googleapis.com/auth/classroom.announcements',
+    'https://www.googleapis.com/auth/classroom.courses',
+    'https://www.googleapis.com/auth/classroom.coursework.me',
+    'https://www.googleapis.com/auth/classroom.coursework.students',
+    'https://www.googleapis.com/auth/classroom.courseworkmaterials',
+    'https://www.googleapis.com/auth/classroom.profile.emails',
+    'https://www.googleapis.com/auth/classroom.profile.emails',
+    'https://www.googleapis.com/auth/classroom.profile.photos',
+    'https://www.googleapis.com/auth/classroom.topics',
+    'https://www.googleapis.com/auth/classroom.rosters',
     'profile',
 ];
 authenticate(scopes)
