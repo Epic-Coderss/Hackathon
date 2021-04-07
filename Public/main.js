9980b9985544eb3db056e689f204eca7b5a36f66
Pranav Kethireddy <pran.kethi@gmail.com>
2021-04-07 09:24:36 -0700

    files

--- Public/main.js
+++ Public/main.js
@@ -1,4 +1,5 @@
-const sqlite3 = require('sqlite3').verbose();
+// import { createRequire } from 'module';
+var require = require('sqlite3');
 let db = new sqlite3.Database('users.db', sqlite3.OPEN_READWRITE, (err) => {
   if (err) {
     console.error(err.message);
@@ -31,13 +32,31 @@ function onSignIn(googleUser) {
 }
 
 function adminClick() {
+  var sqlite3 = require('sqlite3');
+  let db = new sqlite3.Database('users.db', sqlite3.OPEN_READWRITE, (err) => {
+    if (err) {
+      console.error(err.message);
+    }
+    console.log('Connected to the users database.');
+  });
+  let sql = 'SELECT * FROM AdminPerms';
+  db.all(sql, [], (err, rows) => {
+    if (err) {
+      throw err;
+    }
+    rows.forEach((row) => {
+      var usernames = row.Username;
+      var where = row.AdminOf;
+      console.log(usernames);
+      console.log(where)
+    });
+  });
   if (name == usernames && !(where == null)) {
     window.location.replace('https://hackathon-309716.firebaseapp.com/adminPg.html')
   } else {
     console.log('work on this')
   }
 }
-
 
 db.close((err) => {
   if (err) {
